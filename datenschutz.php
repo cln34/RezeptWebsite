4219ada905c090ec7fc17f47e5f6afb09e7a890c
cln34 <162312612+cln34@users.noreply.github.com>
2025-04-11 15:06:12 +0200

    Alle html files zu php geändert um footer und header überall automatisch einfügen zu lassen im näcshten schritt

---
+++ datenschutz.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Rezeptesammlung">
+    <meta name="author" content="Sascha Busse, Christoph Rettig, Colin Bolbas">
+    <link rel="stylesheet" href="css/main.css" />
+    <title>StudiRezepte-Einfach & Günstig</title>
+</head>
+
+
+<body>
+
+    <nav>
+        <div>
+            <a href="index.php">Homepage</a>
+        </div>
+        <div>
+            <a href="anmeldung.php">Anmelden/Abmelden</a>
+        </div>
+        <div>
+            <a href="registrierung.php">Registrieren</a>
+        </div>
+    </nav>
+
+    <main>
+
+        <h1>Datenschutz</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+            sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+
+    </main>
+
+    <footer>
+      <a href="impressum.php">Impressum</a>
+      <a href="datenschutz.php">Datenschutz</a>
+      <a href="nutzungsbedingungen.php">Nutzungsbedingungen</a>
+    </footer>
+</body>
+
+</html>
---
+++ registrierung.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Rezeptesammlung" />
+    <meta
+      name="author"
+      content="Sascha Busse, Christoph Rettig, Colin Bolbas"
+    />
+    <link rel="stylesheet" href="css/main.css" />
+    <title>StudiRezepte-Einfach & Günstig</title>
+  </head>
+
+  <body>
+    <nav>
+      <div>
+        <a href="index.php">Homepage</a>
+      </div>
+      <div>
+        <a href="anmeldung.php">Anmelden/Abmelden</a>
+      </div>
+      <div>
+        <a href="registrierung.php">Registrieren</a>
+      </div>
+    </nav>
+
+    <main>
+      <h1>Registrierung</h1>
+      <form>
+        <div>
+          <input type="text" placeholder="Email Adresse" id="email" />
+        </div>
+        <br />
+        <div>
+          <input type="text" placeholder="Passwort" id="passwort" />
+        </div>
+        <br />
+        <div>
+          <input
+            type="text"
+            placeholder="Passwort wiederholen"
+            id="passwortWDH"
+          />
+        </div>
+        <br />
+      </form>
+      <a href="anmeldung.php">
+            <input type="submit"></a>
+    </main>
+
+    <footer>
+      <a href="impressum.php">Impressum</a>
+      <a href="datenschutz.php">Datenschutz</a>
+      <a href="nutzungsbedingungen.php">Nutzungsbedingungen</a>
+    </footer>
+  </body>
+</html>
