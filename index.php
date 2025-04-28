ac74409f972690bcacc9421ab45646a577886aed
cln34 <162312612+cln34@users.noreply.github.com>
2025-04-28 13:04:14 +0200

    flexbox auf index seite erstellt, noch anklickbar machen die rezepte

--- index.php
+++ index.php
@@ -33,14 +33,85 @@
     <h2>Rezepte:</h2>
     <!-- Flexbox erstellen, um essen nebeneinander darzustellen -->
     <div class="container">
-      <div class="box" id="box1">Pizza</div>
-      <div class="box" id="box2">Burger</div>
-      <div class="box" id="box3">Spaghetti Carbonara</div>
-      <div class="box" id="box4">Spaghetti Bolognese</div>
-      <div class="box" id="box4">Pesto</div>
+
+      <!-- <a href="rezept.php" class="box-link"> -->
+      <div class="box" id="box1">
+        <h2>Pizza:</h2>
+        <img
+          src="images/pizza1.jpg"
+          alt="das ist ein Bild von Pizza"
+          title="Lecker Pizza" />
+        <div class="info-grid">
+          <div>Dauer</div>
+          <div>Schwierigkeit</div>
+          <div>Ungefährer Preis</div>
+          <div>30 min</div>
+          <div>Mittel</div>
+          <div>4€</div>
+
+
+        </div>
+        <p>Kurzbeschreibung: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sed ab eos aspernatur ipsam cum repellendus culpa commodi consequuntur, velit pariatur quod dolores nisi itaque similique repellat provident sequi! Veniam!</p>
+      </div>
+      <!--   </a> -->
+
+      <div class="box" id="box2">
+        <h2>Burger:</h2>
+        <img
+          src="images/burger.jpg"
+          alt="das ist ein Bild von einem Burger"
+          title="Saftiger Burger" />
+        <div class="info-grid">
+          <div>Dauer</div>
+          <div>Schwierigkeit</div>
+          <div>Ungefährer Preis</div>
+          <div>20 min</div>
+          <div>Leicht</div>
+          <div>5€</div>
+        </div>
+        <p>Kurzbeschreibung: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sed ab eos aspernatur ipsam cum repellendus culpa commodi consequuntur, velit pariatur quod dolores nisi itaque similique repellat provident sequi! Veniam!</p>
+      </div>
+
+      <div class="box" id="box3">
+        <h2>Spaghetti Carbonara</h2>
+
+        <div class="info-grid">
+          <div>Dauer</div>
+          <div>Schwierigkeit</div>
+          <div>Ungefährer Preis</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+      <div class="box" id="box4">
+        <h2>Spaghetti Bolognese</h2>
+
+        <div class="info-grid">
+          <div>Dauer</div>
+          <div>Schwierigkeit</div>
+          <div>Ungefährer Preis</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+      <div class="box" id="box4">
+        <h2>Pesto</h2>
+
+        <div class="info-grid">
+          <div>Dauer</div>
+          <div>Schwierigkeit</div>
+          <div>Ungefährer Preis</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
     </div>
-
-
 
   </main>
   <hr><br>
