88a36ae3a8c7abc94ca2f0b13d679e419fb66c3f
cln34 <162312612+cln34@users.noreply.github.com>
2025-04-28 11:43:04 +0200

    Flexbox hinzugefügt zu index hinzugefügt, jetzt noch mit content füllen

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="author" content="Sascha Busse, Christoph Rettig, Colin Bolbas">
   <link rel="stylesheet" href="css/main.css" />
   <title>StudiRezepte-Einfach & Günstig</title>
+  <link href="https://fonts.googleapis.com/css2?family=Tagesschrift&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -30,73 +31,17 @@
       <button>Rezept erstellen</button></a>
     <hr />
     <h2>Rezepte:</h2>
-    beispielausgabe!!!!:
-    <!-- Tabelle erstellen, um essen nebeneinander darzustellen -->
-    <table width="100%">
-      <tr>
-        <!-- erste zzelle für Pizza -->
-        <td width="50%" valign="top">
-        <a href="rezept.php">
-          <h2>Pizza:</h2>
-            <img
-              src="images/pizza1.jpg"
-              alt="das ist ein Bild von Pizza"
-              title="Lecker Pizza"
-              height="300" />
-          </a>
-          <table width="100%" bgcolor="black">
-            <tr bgcolor="grey" align="center">
-              <th width="150">Dauer</th>
-              <th width="150">Schwierigkeit</th>
-              <th width="150">Ungefährer Preis</th>
-            </tr>
-            <tr bgcolor="lightgrey" align="center">
-              <td>30 min</td>
-              <td>Mittel</td>
-              <td>4€</td>
-            </tr>
-          </table>
-          Kurzbeschreibung hier hinschreiben
-        </td>
+    <!-- Flexbox erstellen, um essen nebeneinander darzustellen -->
+    <div class="container">
+      <div class="box" id="box1">Pizza</div>
+      <div class="box" id="box2">Burger</div>
+      <div class="box" id="box3">Spaghetti Carbonara</div>
+      <div class="box" id="box4">Spaghetti Bolognese</div>
+      <div class="box" id="box4">Pesto</div>
+    </div>
 
-        <!-- zweite zelle für Burger -->
-        <td width="50%" valign="top">
-          <h2>Burger:</h2>
 
-          <img
-            src="images/burger.jpg"
-            alt="das ist ein Bild von einem Burger"
-            title="Saftiger Burger"
-            height="300" />
-          <table width="100%" bgcolor="black">
-            <tr bgcolor="grey" align="center">
-              <th width="150">Dauer</th>
-              <th width="150">Schwierigkeit</th>
-              <th width="150">Ungefährer Preis</th>
-            </tr>
-            <tr bgcolor="lightgrey" align="center">
-              <td>20 min</td>
-              <td>Leicht</td>
-              <td>5€</td>
-            </tr>
-          </table>
-          Kurzbeschreibung hier hinschreiben
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h2>Spaghetti Carbonara</h2>
-          <p>hier noch bild einfügem</p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tenetur voluptates corrupti dignissimos dicta perferendis quisquam sequi. Eos, repudiandae itaque praesentium error, aperiam maxime assumenda deserunt voluptatem autem nulla sint.
-        </td>
-        <td>
-          <h2>Spaghetti Bolognese</h2>
-          <p>hier noch bild einfügem</p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, magni neque ratione ad nam quis dolorem aliquam modi tenetur porro recusandae temporibus itaque excepturi facilis aspernatur explicabo, numquam tempore eum!
-        </td>
 
-      </tr>
-    </table>
   </main>
   <hr><br>
   <?php
