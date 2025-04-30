d466295f3fa62e03a6629b329818d7abedfa6de7
Sascha <sascha.busse@uni-oldenburg.de>
2025-04-30 17:03:54 +0200

    button eingefügt

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
   <nav class="navbar">
         <ul>
                 <li> <a href="index.php">Homepage</a> </li>
-                <li> <a href="anmeldung.php">Anmelden/Abmelden</a> </li>
-                <li> <a href="registrierung.php">Registrieren</a> </li>
+                <button> <a href="anmeldung.php">Anmelden/Abmelden</a> </button>
+                <button> <a href="registrierung.php">Registrieren</a> </button>
                 <li> <a href="favoriten.php">Favoriten</a> </li>
+
+        
         </ul>
+
+        
 
   </nav>
