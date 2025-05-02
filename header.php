225e9e91d020df35d0ca7eaeb1063bdd1cf2ea02
Sascha <sascha.busse@uni-oldenburg.de>
2025-05-02 16:16:44 +0200

    1123

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
                 <li> <a href="rezeptErstellen.php">Rezept erstellen</a> </li>
                 <li> <a href="userliste.php">Userliste</a> </li>
 
-                <button> <a href="anmeldung.php">Anmelden/Abmelden</a> </button>
-                <button> <a href="registrierung.php">Registrieren</a> </button>
+              <!--  <button> <a href="anmeldung.php">Anmelden/Abmelden</a> </button>
+                <button> <a href="registrierung.php">Registrieren</a> </button> -->
         </ul>
   </nav>
