9db36cfb07e1362107168715199b95a89308c9ad
Sascha <sascha.busse@uni-oldenburg.de>
2025-05-03 10:42:37 +0200

    Rezept erstellen Seite komplett eingepflegt

--- rezeptErstellen.php
+++ rezeptErstellen.php
@@ -10,71 +10,6 @@
     <title>StudiRezepte-Einfach & Günstig</title>
 </head>
 
-<!-- <body>
-   
-    <hr>
-    <br>
-    <main>
-        <h1>Rezept erstellen</h1>
-        <hr>
-        <br>
-        <form>
-            <p>Rezeptitel eingeben:</p>
-            <div>
-                <label for="Rezepttitel"><input type="text" placeholder="Rezepttitel" name="Rezepttitel" required></label>
-            </div>
-            <br>
-            <hr>
-            <p>Bild einfügen:</p>
-            <div>
-                <label for="Bild"><input type="file" placeholder="Bild einfügen" name="Bild" required></label>
-            </div>
-            <br>
-            <hr>
-            <p>Zutaten eingeben: </p>
-            <div>
-                <label for="Zutaten"><textarea placeholder="Zutaten" name="Zutaten" required></textarea></label>
-            </div>
-            <br>
-            <hr>
-            <p>Kochanleitung eingeben:</p>
-            <div>
-                <label for="Kochanleitung"><textarea placeholder="Kochanleitung" name="Kochanleitung" required></textarea></label>
-            </div>
-            <br>
-            <hr>
-            <p>Kurzbeschreibung eingeben:</p>
-            <div>
-                <label for="Kurzbeschreibung"><textarea placeholder="Kurzbeschreibung" name="Kurzbeschreibung" required></textarea></label>
-            </div>
-            <br>
-            <hr>
-            <p>Schwierigkeit angeben: </p>
-            <div>
-                <label for="Schwierigkeit"><select placeholder="Schwierigkeit" name="Schwierigkeit" required>
-                        <option value="leicht">leicht</option>
-                        <option value="mittel">mittel</option>
-                        <option value="schwer">schwer</option>
-                    </select>
-
-                </label>
-            </div>
-            <br>
-            <hr>
-            <p>Preis angeben (max 50€):</p>
-            <div>
-                <label for="Preis"><input type="number" placeholder="Preis" name="Preis" required max="50" min="0"></textarea></label>
-            </div>
-            <br>
-            <hr>
-            <input type="submit">
-
-        </form>
-    </main>
-    <hr><br>
-
-</body> >-->
-
 <body>
 
     <?php
@@ -82,8 +17,8 @@
     ?>
 
     <main class="rezeptErstellen">
-        <form action="rezeptSpeichern.php" method="post" enctype="multipart/form-data">
-            <h2>Rezept einreichen</h2>
+        <form action="index.php">
+            <h2>Rezept erstellen</h2>
 
             <label for="titel">Rezepttitel</label>
             <input type="text" id="titel" name="titel" required />
@@ -106,6 +41,9 @@
             <option value="mittel">mittel</option>
             <option value="schwer">schwer</option>
             </select>
+
+            <label for="preis">Preis angeben (max 50€):</label>
+            <input type="number" id="preis" name="preis" min="0" max="50" step="0.01" required />
 
             <input type="submit" value="Rezept speichern" />
         </form>
