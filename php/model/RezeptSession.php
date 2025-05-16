775310d7609e6b5be2c3d917b9f36042a621638a
Sascha <sascha.busse@uni-oldenburg.de>
2025-05-16 18:52:38 +0200

    Methoden bei RezeptSession ergänzt

--- php/model/RezeptSession.php
+++ php/model/RezeptSession.php
@@ -12,27 +12,62 @@ class RezeptSession implements RezeptDAO{
     }
     private $entries = array();
 
+	private function __construct() {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        if (isset($_SESSION["entries"])) {
+            $this->entries = unserialize($_SESSION["entries"]);
+        } else {
+            // Beispiel-Daten zum Testen
+            $this->entries[0] = new RezeptEintrag(0, "Spaghetti Carbonara", "koch@example.com", "Leckere Pasta", 30, "Mittel", "Günstig", "Spaghetti, Eier, Speck", "Kochen & mischen", "bild1.jpg");
+            $this->entries[1] = new RezeptEintrag(1, "Pfannkuchen", "backen@example.com", "Schneller Snack", 20, "Einfach", "Sehr günstig", "Mehl, Eier, Milch", "Alles verrühren und braten", "bild2.jpg");
+            $_SESSION["entries"] = serialize($this->entries);
+            $_SESSION["nextId"] = 2;
+        }
+    }
+
 	public function createEntry($id, $title, $email, $kurzbeschreibung, $dauer, $schwierigkeit, $preis, $zutaten, $anleitung, $bild) {
         $this->entries[$_SESSION["nextId"]] = new RezeptEintrag($_SESSION["nextId"], $title, $email, $kurzbeschreibung, $dauer, $schwierigkeit, $preis, $zutaten, $anleitung, $bild);
         $_SESSION["nextId"] = $_SESSION["nextId"] + 1;
         $_SESSION["entries"] = serialize($this->entries);
 	}
 
-	public function readEntry($id) {
-		// TODO: Implement readEntry() method.
-	}
+	   public function readEntry($id) {
+        foreach ($this->entries as $entry) {
+            if ($entry->getId() == $id) {
+                return $entry;
+            }
+        }
+        throw new Exception("Eintrag mit ID $id nicht gefunden.");
+    }
 
 	public function updateEntry($id, $title, $email, $kurzbeschreibung, $dauer, $schwierigkeit, $preis, $zutaten, $anleitung, $bild) {
-		// TODO: Implement updateEntry() method.
+		 foreach ($this->entries as $entry) {
+            if ($entry->getId() == $id) {
+                // Neue RezeptEintrag-Instanz oder eine update-Methode?
+                $this->entries[$id] = new RezeptEintrag($id, $title, $email, $kurzbeschreibung, $dauer, $schwierigkeit, $preis, $zutaten, $anleitung, $bild);
+                $_SESSION["entries"] = serialize($this->entries);
+                return $this->entries[$id];
+            }
+        }
+        throw new Exception("Eintrag mit ID $id nicht gefunden.");
 	}
 
 	public function deleteEntry($id) {
-		// TODO: Implement deleteEntry() method.
+		if (isset($this->entries[$id])) {
+            unset($this->entries[$id]);
+            $this->entries = array_values($this->entries); // Reindizieren
+            $_SESSION["entries"] = serialize($this->entries);
+        } else {
+            throw new Exception("Eintrag mit ID $id existiert nicht.");
+        }
 	}
 
-	public function getEntries() {
-		// TODO: Implement getEntries() method.
-	}
+	 public function getEntries() {
+        return $this->entries;
+    }
 
 }
 ?>
