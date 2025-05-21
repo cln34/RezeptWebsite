f4676ae37f92bdb25373ff567104909859f6b2be
Sascha <sascha.busse@uni-oldenburg.de>
2025-05-21 15:44:34 +0200

    UserSession angelegt

--- php/model/UserDAO.php
+++ php/model/UserDAO.php
@@ -5,16 +5,16 @@ class MissingEntryException extends Exception {}
 interface UserDAO
 {
 	
-	public function createUser($email, $passwort, $rolle);
+	public function createUser($email, $passwort);
 
 	
-	public function readUser($email);
+	public function readUser($id);
 
 	
-	public function updateUser($email, $passwort, $rolle);
+	public function updateUser($id, $email, $passwort, $rolle);
 
 	
-	public function deleteUser($email);
+	public function deleteUser($id);
 
 
 	public function getUsers();
--- php/model/UserEintrag.php
+++ php/model/UserEintrag.php
@@ -1,16 +1,24 @@
 <?php
 class UserEintrag
 {
+    private $id;
     private $email;
     private $password;
     private $rolle;
 
-    public function __construct($email, $password, $rolle)
+    public function __construct($id, $email, $password, $rolle)
     {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
         $this->rolle = $rolle;
     }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
     public function getEmail()
     {
         return $this->email;
