625c15c7f1b81b5ee47ed7be7fa53af8f6bf04dc
Sascha <sascha.busse@uni-oldenburg.de>
2025-06-01 14:35:14 +0200

    User werden bei Registrierung in Datenbank gespeichert statt in Session

--- php/model/User.php
+++ php/model/User.php
@@ -1,5 +1,6 @@
 <?php
 require_once "php/model/UserSession.php";
+require_once "php/model/UserPDOSQLite.php";
 /*
  * je nachdem ob die Webanwendung mit der Dummy-Fix- oder der Datenbank-Implementierung laufen soll,
  * ist die Implementierung der Methode getInstance die einzige Stelle im gesamten Code, an der eine
@@ -8,8 +9,8 @@ require_once "php/model/UserSession.php";
 
 class User{
     public static function getInstance(){
-        return UserSession::getInstance(); //dummy fix
-        //return UserDB::getInstance(); // Datenbank
+        //return UserSession::getInstance(); //dummy fix
+        return UserPDOSQLite::getInstance(); // Datenbank
     }
 }
 ?>
