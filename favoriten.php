2e3ff186d3924da853731fdfbe66c948cba41dfe
Sascha <sascha.busse@uni-oldenburg.de>
2025-06-02 15:28:46 +0200

    email anzeige hinzugefügt

--- favoriten.php
+++ favoriten.php
@@ -5,9 +5,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 if (isset($_SESSION["email"])) {
     echo $_SESSION["email"];
 }
-if (isset($_SESSION["email"])) {
-    echo $_SESSION["email"];
-}
 ?>
 <?php
 require_once "php/include/head.php";
