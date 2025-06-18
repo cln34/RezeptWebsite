d8bf35f820980cea840e686e120f9f6b8b96aa35
colin <colin.bolbas@uni-oldenburg.de>
2025-06-18 17:48:39 +0200

    passwort wird validiert

--- php/view/user-registrieren.php
+++ php/view/user-registrieren.php
@@ -37,7 +37,7 @@ require_once "php/include/head.php";
       <input type="email" id="email" name="email" placeholder="z. B. max@mustermann.de" required />
 
       <label for="passwort">Passwort</label>
-      <input type="password" id="passwort" name="passwort" placeholder="Passwort" required />
+      <input type="password" id="passwort" name="passwort" placeholder="Passwort" minlength="8" maxlength="20" required />
 
       <label for="passwortWDH">Passwort wiederholen</label>
       <input type="password" id="passwortWDH" name="passwortWDH" placeholder="Passwort wiederholen" required />
@@ -53,6 +53,44 @@ require_once "php/include/head.php";
   <?php
   include_once "php/include/footer.php"
   ?>
+
+  <script>
+    const passwortFeld = document.getElementById("passwort");
+    const wdhPasswortFeld = document.getElementById("passwortWDH");
+
+    function validateWdhPasswortFeld() {
+      const pwWDH = wdhPasswortFeld.value;
+      const pw = passwortFeld.value;
+
+      wdhPasswortFeld.classList.remove("input-valid", "input-invalid");
+
+      if (pwWDH === "") {
+        // Noch nichts eingegeben, keine Klasse setzen
+        return;
+      }
+      if (pwWDH === pw) {
+        wdhPasswortFeld.classList.add("input-valid");
+      } else {
+        wdhPasswortFeld.classList.add("input-invalid");
+      }
+    }
+
+    passwortFeld.addEventListener("input", function() {
+      const pw = this.value;
+      this.classList.remove("input-valid", "input-invalid");
+
+      if (pw.length < 8) {
+        this.classList.add("input-invalid");
+      } else {
+        this.classList.add("input-valid");
+      }
+
+      // Wdh-Passwortfeld sofort mitprüfen!
+      validateWdhPasswortFeld();
+    });
+
+    wdhPasswortFeld.addEventListener("input", validateWdhPasswortFeld);
+  </script>
 </body>
 
 </html>
